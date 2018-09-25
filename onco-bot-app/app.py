6b9fd086943123998cba837ea1a4adf24492d38a
Alyss Flynn <alyss.flynn@me.com>
2018-09-25 12:14:58 -0500

    app setup

--- onco-bot-app/app.py
+++ onco-bot-app/app.py
@@ -115,5 +115,5 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     app.run(debug=True)
